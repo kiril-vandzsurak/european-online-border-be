eb976bd78068e16c671b85e7a9d3f6bdfe93cdb0
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-03-02 17:07:30 +0100

    implementing password hashing and credentialsCheck after login

--- src/api/users/model.js
+++ src/api/users/model.js
@@ -49,4 +49,39 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
+userSchema.pre("save", async function (next) {
+  const currentUser = this;
+  if (currentUser.isModified("password")) {
+    const plainPW = currentUser.password;
+    const hash = await bcrypt.hash(plainPW, 11);
+    currentUser.password = hash;
+  }
+  next();
+});
+
+userSchema.methods.toJSON = function () {
+  const userDocument = this;
+  const user = userDocument.toObject();
+
+  delete user.password;
+  delete user.createdAt;
+  delete user.updatedAt;
+  delete user.__v;
+  return user;
+};
+
+userSchema.static("checkCredentials", async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const passportMatch = await bcrypt.compare(password, user.password);
+    if (passportMatch) {
+      return user;
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+});
+
 export default model("User", userSchema);
